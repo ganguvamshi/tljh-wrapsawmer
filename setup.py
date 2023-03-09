b939a592c2fa9b4d4ffaab2e908c2f4f6aa74180
Vamshidhar Gangu <ganguvamshi@gmail.com>
2023-03-09 11:48:33 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup (
     author_email = 'ganguvamshi@gmail.com',
     version="0.0.1",
     license="GPL-3.0 license",
-    py_modules=["tljh_wrapspawner"],
     url="https://github.com/ganguvamshi/tljh-wrapsawner",
     entry_points = {"tljh": ["wrapspawner = tljh_wrapspawner"]},
 )
