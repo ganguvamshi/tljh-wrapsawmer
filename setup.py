dfe0044091855798cefebba4d22cba70fb2dff0c
Vamshidhar Gangu <ganguvamshi@gmail.com>
2023-03-09 12:02:41 +0800

    added pymodule

--- setup.py
+++ setup.py
@@ -9,4 +9,5 @@ setup (
     license="GPL-3.0 license",
     url="https://github.com/ganguvamshi/tljh-wrapsawner",
     entry_points = {"tljh": ["wrapspawner = tljh_wrapspawner"]},
+    py_modules=['tljh_wrapspawner']
 )
