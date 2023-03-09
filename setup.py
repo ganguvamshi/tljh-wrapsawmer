e45314e3201c82826f642dfe248f1a53749ce4eb
Vamshidhar Gangu <ganguvamshi@proteona.com>
2023-03-09 11:19:00 +0800

    setup scripts and tljh plugin installation are ready

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name="tljh-wrapspawner",
+    author="vamshidhar gangu",
+    author_email = 'ganguvamshi@gmail.com'
+    version="0.0.1"
+    license="GPL-3.0 license",
+    long_description=read('README.md'),
+    packages=find_packages(),
+    py_modules=["tljh_wrapspawner"],
+    url="https://github.com/ganguvamshi/tljh-wrapsawner",
+    entry_points = {"tljh": ["wrapspawner = tljh_wrapspawner"]},
+)
---
+++ tljh_wrapspwaner.py
@@ -0,0 +1,61 @@
+from tljh.hooks import hookimpl
+import subprocess
+
+
+@hookimpl
+def tljh_config_post_install(config):
+    """
+    Set JupyterLab to default
+    """
+    user_environment = config.get('user_environmnet', {})
+    user_environment['default_app'] = user_environment.get('default_app', 'jupyterlab')
+    
+    config['user_environment'] = user_environment
+
+@hookimpl
+def tljh_post_install():
+    """
+    Install DockerSpawner, WrapSpawner and setup the notebook0
+    """
+    def install_dockerspawner():
+        subprocess.call("sudo /opt/tljh/hub/bin/python3 -m pip install dockerspawner jupyter_client", shell=True)
+
+
+    def install_wrap_spawner():
+        subprocess.call("sudo /opt/tljh/hub/bin/python3 -m pip install wrapspawner", shell=True)
+        tljh_use_wrapspawner()
+    
+    def tljh_use_wrapspawner():
+        
+        # create the wrapspawner config file
+        f = open('/opt/tljh/config/jupyterhub_config/wrapspawner_tljh_config.py', 'w')
+
+        # add the details to configure dockerspaner
+        contents= [
+            "c.JupyterHub.spawner_class = 'wrapspawner.ProfilesSpawner'",
+            "c.Spawner.http_timeout = 120",
+            "c.ProfileSpawner.profiles = [('Host process','local','systemdspawner.SystemdSpawner') ," ,
+            "\t('Docker (singlecell)' ,'singlecell', 'dockerspawner.SystemUserSpawner', dict(image=\"rnakato/singlecell_jupyter:latest\") ),",
+            "\t('Docker (Renv)' ,'datascience', 'dockerspawner.SystemUserSpawner', dict(image=\"jupyter/datascience-notebook:r-4.0.3\") ) ],",
+            "c.DockerSpawner.image_whitelist = ['rnakato/singlecell_jupyter:latest','jupyter/datascience-notebook:r-4.0.3']",
+            "from jupyter_client.localinterfaces import public_ips",
+            "c.JupterHub.hub_ip = public_ips()[0]",
+            "c.DockerSpawner.name_template = '{prefix}-{username}-{servername}'"
+        ]
+
+        # add to the config file
+        for line in contents:
+            f.write(line)
+            f.write("\n")
+        f.close()
+        get_docker_image()
+
+    def get_docker_image():
+        subprocess.call("sudo docker pull jupyter/datascience-notebook:r-4.0.3", shell=True)
+        restart_tljh()
+
+    # and the restart TLJH and rebuild jupyterlab
+    def restart_tljh():
+        subprocess.call("sudo tljh-config reload", shell=True)
+    
+
